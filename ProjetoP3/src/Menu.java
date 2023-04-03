3d72a2167e747d1137b8639229b93cf8ae0477ee
Graziela de Oliveira <94752637+Graazi@users.noreply.github.com>
2023-04-03 09:51:26 -0300

    switchcase

--- ProjetoP3/src/Menu.java
+++ ProjetoP3/src/Menu.java
@@ -1,3 +1,15 @@
 public class Menu {
     
+    private String user;
+    private String adm;
+    private String cliente;
+    
+    switch (){
+        case 1:
+            break;
+        defalt:
+            break;
+    
+
+    }
 }
