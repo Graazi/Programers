0522c2584bf50296604971e30940e279ab7e41a2
Graziela de Oliveira <94752637+Graazi@users.noreply.github.com>
2023-04-13 16:27:24 -0300

    getterssetters

--- ProjetoP3/src/estoque/Produto.java
+++ ProjetoP3/src/estoque/Produto.java
@@ -5,4 +5,29 @@ public class Produto {
     private int quantEst;
     private int [] codProd;
     
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public double getPreco() {
+        return preco;
+    }
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    public int getQuantEst() {
+        return quantEst;
+    }
+    public void setQuantEst(int quantEst) {
+        this.quantEst = quantEst;
+    }
+    public int[] getCodProd() {
+        return codProd;
+    }
+    public void setCodProd(int[] codProd) {
+        this.codProd = codProd;
+    }
+    
 }
--- ProjetoP3/src/user/cliente.java
+++ ProjetoP3/src/user/cliente.java
@@ -4,4 +4,24 @@ public class cliente {
     private String nome;
     private int cpf;
     private String senha;
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getCpf() {
+        return cpf;
+    }
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
 }
