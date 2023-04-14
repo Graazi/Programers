8623503fc3659b4228ae8a3f7a44f041a2ea1b2e
Graziela de Oliveira <94752637+Graazi@users.noreply.github.com>
2023-04-14 11:56:38 -0300

    menucarrinho

--- ProjetoP3/src/Menu.java
+++ ProjetoP3/src/Menu.java
@@ -4,12 +4,27 @@ public class Menu {
     private String adm;
     private String cliente;
     
-    switch (){
-        case 1:
-            break;
-        defalt:
-            break;
     
+}
 
-    }
+    
+public static void exibeMenu() {
+        System.out.println("Opções:");
+        System.out.println("1 - Cadastro de Cliente");
+        System.out.println("2 - Adcionar livro do carrinho");
+        System.out.println("3 - Remover livro do carrinho");
+        System.out.println("4 - Ver Carrinho");
+        System.out.println("5 - Exibir a Parcial");
+        System.out.println("6 - Fechar o pedido e realizar pagamento");
+        System.out.println("7 - Cancelar Pedido");
+        System.out.println("0 - Sair");
+}
+    
+public static void exibeMenuDePagamento() {
+        System.out.println(" ");
+        System.out.println("Escolha uma forma de pagamento: ");
+        System.out.println("1- Crédito");
+        System.out.println("2- Débito");
+        System.out.println("3- Pix");
+
 }
--- ProjetoP3/src/pedido/Carrinho.java
+++ ProjetoP3/src/pedido/Carrinho.java
@@ -1,4 +1,40 @@
 package pedido;
+
 public class Carrinho {
+
+    private String livro;
+    private int numero;
+    private double preco;
+
+    public void pedido (String livro, int numero, double preco) {
+        this.livro = livro;
+        this.numero = numero;
+        this.preco = preco;
+    }
+
+    public String getLivro() {
+        return livro;
+    }
+
+    public void setLivro(String livro) {
+        this.livro = livro;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
     
+
 }
