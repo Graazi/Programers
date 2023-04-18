2aee565c20189ed9fa5878f446877c1a2f694e6f
Graziela de Oliveira <grazielaoliveiragg@gmail.com>
2023-04-18 16:38:50 +0000

    formatação

--- ProjetoP3/src/Menu.java
+++ ProjetoP3/src/Menu.java
@@ -1,18 +1,15 @@
 import java.util.Scanner;
 
-
 import user.cliente;
 
 public class Menu {
-        Scanner in = new Scanner (System.in);
+        Scanner in = new Scanner(System.in);
 
-        
         String nomeCliente;
         int cpf, cliente;
         String senha;
-        
 
-         {
+        {
                 exibeMenu();
                 cliente = in.nextInt();
                 in.nextLine();
@@ -27,29 +24,26 @@ public class Menu {
                                 
                                 break;
                 }
-    
 
+        public static void exibeMenu() {
+                System.out.println("Opções:");
+                System.out.println("1 - Cadastro de Cliente");
+                System.out.println("2 - Adcionar livro do carrinho");
+                System.out.println("3 - Remover livro do carrinho");
+                System.out.println("4 - Ver Carrinho");
+                System.out.println("5 - Exibir a Parcial");
+                System.out.println("6 - Fechar o pedido e realizar pagamento");
+                System.out.println("7 - Cancelar Pedido");
+                System.out.println("0 - Sair");
+        }
 
-    
-public static void exibeMenu() {
-        System.out.println("Opções:");
-        System.out.println("1 - Cadastro de Cliente");
-        System.out.println("2 - Adcionar livro do carrinho");
-        System.out.println("3 - Remover livro do carrinho");
-        System.out.println("4 - Ver Carrinho");
-        System.out.println("5 - Exibir a Parcial");
-        System.out.println("6 - Fechar o pedido e realizar pagamento");
-        System.out.println("7 - Cancelar Pedido");
-        System.out.println("0 - Sair");
-}
-    
-public static void exibeMenuDePagamento() {
-        System.out.println(" ");
-        System.out.println("Escolha uma forma de pagamento: ");
-        System.out.println("1- Crédito");
-        System.out.println("2- Débito");
-        System.out.println("3- Pix");
+        public static void exibeMenuDePagamento() {
+                System.out.println(" ");
+                System.out.println("Escolha uma forma de pagamento: ");
+                System.out.println("1- Crédito");
+                System.out.println("2- Débito");
+                System.out.println("3- Pix");
 
-}
+        }
 
 }
