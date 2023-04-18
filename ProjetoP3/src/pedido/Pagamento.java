3ba595503c17dba418920ebb5d9a6ab19d9ba1b3
Graziela de Oliveira <grazielaoliveiragg@gmail.com>
2023-04-18 16:54:11 +0000

    pagamento

--- ProjetoP3/src/pedido/Pagamento.java
+++ ProjetoP3/src/pedido/Pagamento.java
@@ -1,5 +1,36 @@
 package pedido;
 
 public class Pagamento {
+
+    private String boleto;
+    private String pix;
+    private String cartao;
+
+    
+    public Pagamento(String boleto, String pix,String cartao) {
+        this.boleto = boleto;
+        this.pix = pix;
+        this.cartao = cartao;
+        
+    }
+
+    public String getBoleto() {
+        return boleto;
+    }
+    public void setBoleto(String boleto) {
+        this.boleto = boleto;
+    }
+    public String getPix() {
+        return pix;
+    }
+    public void setPix(String pix) {
+        this.pix = pix;
+    }
+    public String getCartão() {
+        return cartão;
+    }
+    public void setCartão(String cartão) {
+        this.cartão = cartão;
+    }
     
 }
