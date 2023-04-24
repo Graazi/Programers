d9fb73b5e086d2dd1623c2b54d8a972579401a34
Graziela de Oliveira <grazielaoliveiragg@gmail.com>
2023-04-24 15:07:24 +0000

    estoque

--- ProjetoP3/src/estoque/Estoque.java
+++ ProjetoP3/src/estoque/Estoque.java
@@ -3,7 +3,37 @@ package estoque;
 public abstract class Estoque {
     private String nome;
     private int quantidade;
-    private int [] codProduto;
-    
+    private int codProduto;
+
+    public void estoque (String nome, int quantidade, int codProduto) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.codProduto = codProduto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public int getCodProduto() {
+        return codProduto;
+    }
+
+    public void setCodProduto(int codProduto) {
+        this.codProduto = codProduto;
+    }
+
     
 }
