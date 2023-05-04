b82db0251b050d77cc905b930cda739c5c7cc261
Graziela de Oliveira <grazielaoliveiragg@gmail.com>
2023-05-04 15:17:44 +0000

    adm

--- ProjetoP3/src/user/adm.java
+++ ProjetoP3/src/user/adm.java
@@ -1,5 +1,20 @@
 package user;
 
 public class adm {
+    private String login;
+    private String senha;
+    
+    public String getLogin() {
+        return login;
+    }
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
     
 }
