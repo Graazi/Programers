0b2f5e6dc46b7800fca90d85a549eddc89526b52
Graziela de Oliveira <grazielaoliveiragg@gmail.com>
2023-05-11 13:56:08 +0000

    estoque

--- ProjetoP3/src/Estoque.java
+++ ProjetoP3/src/Estoque.java
@@ -1,3 +1,53 @@
 public class Estoque {
     //lista de estoque  e procurar o pedido;
+    private String nome;
+    private int codigo;
+   
+   
+   
+    public Estoque(String nome, int codigo) {
+        this.nome = nome;
+        this.codigo = codigo;
+    }
+
+
+
+    
+    public void listaDePedidos() {
+        
+        
+    }
+    
+    
+    
+    
+    public void procurarPedido(int numeroPedido) {
+        
+        
+    }
+
+
+
+    public String getNome() {
+        return nome;
+    }
+
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    
 }
