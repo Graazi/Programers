411ffe0b25607377d5dd404261168dfeb167aa5d
Graziela de Oliveira <grazielaoliveiragg@gmail.com>
2023-05-19 15:10:19 +0000

    pagamento/polimorfismo

--- ProjetoP3/src/pagamento/Boleto.java
+++ ProjetoP3/src/pagamento/Boleto.java
@@ -1,5 +1,46 @@
 package pagamento;
 
-public class Boleto {
-    
+public class Boleto implements ValidarPagamento {
+    private double valor;
+    private boolean boletoPago;
+
+    public Boleto(double valor, boolean boletoPago) {
+        this.valor = valor;
+        this.boletoPago = boletoPago;
+    }
+
+
+    @Override
+    public boolean statusDoPagamento() {
+      
+        if(boletoPago){
+
+            return true;
+            
+        } else {
+            return false;
+        }
+
+    }
+
+
+    public double getValor() {
+        return valor;
+    }
+
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+
+    public boolean isBoletoPago() {
+        return boletoPago;
+    }
+
+
+    public void setBoletoPago(boolean boletoPago) {
+        this.boletoPago = boletoPago;
+    }
+
 }
