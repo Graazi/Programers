8b29295ac113443fd1f1447b8cd7ab457f53a1c4
Graziela de Oliveira <grazielaoliveiragg@gmail.com>
2023-05-19 14:56:01 +0000

    pagamento

--- ProjetoP3/src/pagamento/Debito.java
+++ ProjetoP3/src/pagamento/Debito.java
@@ -1,5 +1,20 @@
 package pagamento;
 
-public class Debito {
+public class Debito implements ValidarPagamento{
+
+    private String bandeira;
+    private double valor;
+    private boolean cartaoAceito;
+    
+    @Override
+    public boolean statusDoPagamento() {
+        if(cartaoAceito){
+
+            return true;
+            
+        }else{
+            return false;
+        }
+    }
     
 }
--- ProjetoP3/src/pagamento/Pix.java
+++ ProjetoP3/src/pagamento/Pix.java
@@ -1,5 +1,31 @@
 package pagamento;
 
-public class Pix {
+public class Pix implements ValidarPagamento{
+
+    
+    private boolean pixRealizado;
+    private String bandeira;
+    private double valor;
+
+   
+    public Pix(String bandeira, double valor, boolean pixRealizado) {
+        this.bandeira = bandeira;
+        this.valor = valor;
+        this.pixRealizado = pixRealizado;
+    }
+
+
+    @Override
+    public boolean statusDoPagamento() {
+      
+        if(pixRealizado){
+
+            return true;
+            
+        }else{
+            return false;
+        }
+
+    }
     
 }
