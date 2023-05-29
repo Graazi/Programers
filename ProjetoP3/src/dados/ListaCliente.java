b115dbb31bbe3a34f50519728ba62f1187027320
Graziela de Oliveira <grazielaoliveiragg@gmail.com>
2023-05-29 14:49:19 +0000

    livraria

--- ProjetoP3/src/dados/ListaCliente.java
+++ ProjetoP3/src/dados/ListaCliente.java
@@ -1,3 +1,21 @@
+import java.util.ArrayList;
+
 public class ListaCliente {
-    
+    private ArrayList<Paciente> clientes;
+
+    public ListaCliente(ArrayList<Paciente> clientes) {
+        this.clientes  = clientes;
+    }
+
+    public ListaCliente() {
+        clientes = new ArrayList<>();
+    }
+
+    public ArrayList<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(ArrayList<Cliente> clientes) {
+        this.clientes = clientes;
+    }
 }
