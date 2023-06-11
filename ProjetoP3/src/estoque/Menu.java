a0a5b84a07c621147e267f767c72b9cdf6b30841
Graziela de Oliveira <grazielaoliveiragg@gmail.com>
2023-06-11 20:37:57 +0000

    livraria

--- ProjetoP3/src/estoque/Menu.java
+++ ProjetoP3/src/estoque/Menu.java
@@ -68,8 +68,6 @@ public class Menu {
             System.out.println("3 - Remover Produto do Carrinho");
             System.out.println("4 - Exibir Carrinho de Compras");
             System.out.println("5 - Área Administrativa");
-            System.out.println("6 - Adicionar Produto (Área Administrativa)");
-            System.out.println("7 - Remover Produto (Área Administrativa)");
             System.out.println("8 - Pagar");
             System.out.println("0 - Sair");
             System.out.print("Digite a opção desejada: ");
@@ -165,7 +163,9 @@ public class Menu {
         public static void exibirMenuAreaAdministrativa() {
             System.out.println("======== Área Administrativa ========");
             System.out.println("1 - Listar Produtos");
-            System.out.println("2 - Outra Funcionalidade");
+            System.out.println("2 - Adicionar Produto (Área Administrativa)");
+            System.out.println("3 - Remover Produto (Área Administrativa)");
+            System.out.println("4 - Outra Funcionalidade");
             System.out.println("0 - Voltar ao Menu Principal");
             System.out.print("Digite a opção desejada: ");
         }
